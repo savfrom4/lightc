40a289bace786b2cd1e1b9d92587c93a7c52cdef
silk <savfrom4@gmail.com>
2025-03-27 07:38:01 +0100

    farts

--- include/lc_compiler.h
+++ include/lc_compiler.h
@@ -1,4 +1,24 @@
 #ifndef LC_COMPILER
 #define LC_COMPILER
+#include "lc_types.h"
+
+typedef enum
+{
+    LCTK_TYPE,
+    LCTK_KEYWORD,
+    LCTK_IDENTIFIER,
+    LCTK_OPERATOR,
+    LCTK_LITERAL,
+    LCTK_DELIMITER
+} lc_token_type;
+
+typedef struct
+{
+    lc_token_type type;
+    lc_string *value;
+} lc_token;
+
+lc_list *lc_token_parse(const lc_string *code);
+lc_string *lc_token_name(lc_token_type type);
 
 #endif
